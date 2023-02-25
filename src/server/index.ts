0b3d8330a7c5d62b150eb4371c855c041de37781
DI3BOSS <diegofbermudez@gmail.com>
2023-02-25 01:28:12 +0100

    Add usersRouter to middlewares chain index

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,12 +1,14 @@
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
+import usersRouter from "./routers/usersRouters.js";
 
 export const app = express();
 
 app.disable("x-powered-by");
 
 app.use(morgan("dev"));
-
 app.use(express.json());
 app.use(cors());
+
+app.use("/users", usersRouter);
