9ff6e2cc24c11068d15d8398632a67620a3fcd33
DI3BOSS <diegofbermudez@gmail.com>
2023-02-25 20:32:57 +0100

    Implement multerMiddleware and install uuid

---
+++ src/server/middlewares/multerMiddleware.ts
@@ -0,0 +1,17 @@
+import multer from "multer";
+import { v4 as uuidv4 } from "uuid";
+
+const fileNamePrefix = uuidv4;
+
+const storage = multer.diskStorage({
+  destination(req, file, callback) {
+    callback(null, "uploads/avatars/");
+  },
+  filename(req, file, callback) {
+    callback(null, `${fileNamePrefix()}-${file.originalname}`);
+  },
+});
+
+const upload = multer({ storage });
+
+export default upload;
--- src/server/routers/usersRouters.ts
+++ src/server/routers/usersRouters.ts
@@ -1,13 +1,19 @@
 import { Router } from "express";
 import { getUsers, registerUser } from "../controllers/usersControllers.js";
+import upload from "../middlewares/multerMiddleware.js";
 
 const getUsersEndpoint = "/";
 const registerUserEndpoint = "/register";
+const userProfileImage = "avatar";
 
 // eslint-disable-next-line new-cap
 const usersRouter = Router();
 
 usersRouter.get(getUsersEndpoint, getUsers);
-usersRouter.post(registerUserEndpoint, registerUser);
+usersRouter.post(
+  registerUserEndpoint,
+  upload.single(userProfileImage),
+  registerUser
+);
 
 export default usersRouter;
