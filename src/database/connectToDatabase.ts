a1e40bafb08c008d483bcd07e97f3d6f1ab4f8d1
DI3BOSS <diegofbermudez@gmail.com>
2023-02-25 01:29:52 +0100

    Do minor fixes on magic strings and getUsersEndpoint variable name

--- src/database/connectToDatabase.ts
+++ src/database/connectToDatabase.ts
@@ -1,12 +1,14 @@
 import mongoose from "mongoose";
 
+const databaseConnectionError = "Error while connecting to data base";
+
 const connectToDatabase = async (url: string) => {
   mongoose.set("strictQuery", false);
 
   try {
     await mongoose.connect(url);
   } catch (error) {
-    throw new Error("Error while connecting to data base");
+    throw new Error(databaseConnectionError);
   }
 };
 
--- src/server/routers/usersRouters.ts
+++ src/server/routers/usersRouters.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import getUsers from "../controllers/usersControllers.js";
 
-const usersEndpoint = "/users";
+const getUsersEndpoint = "/";
 
 const usersRouter = Router();
-usersRouter.get(usersEndpoint, getUsers);
+
+usersRouter.get(getUsersEndpoint, getUsers);
 
 export default usersRouter;
