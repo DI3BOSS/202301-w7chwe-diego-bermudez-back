c33be5450c4909eda05b5655d1588a08f2102436
DI3BOSS <diegofbermudez@gmail.com>
2023-02-27 01:46:28 +0100

    Test the loginUser middleware function

--- src/server/controllers/usersControllers.test.ts
+++ src/server/controllers/usersControllers.test.ts
@@ -1,12 +1,14 @@
 import request from "supertest";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 import { MongoMemoryServer } from "mongodb-memory-server";
 import mongoose from "mongoose";
-import { type Response, type Request } from "express";
+import { type Response, type Request, json } from "express";
 import connectToDatabase from "../../database/connectToDatabase.js";
 import User from "../../database/models/User.js";
-import { type UsersStructure } from "../../types.js";
+import { UserStructure, type UsersStructure } from "../../types.js";
 import { app } from "..";
-import { getUsers } from "./usersControllers.js";
+import { getUsers, loginUser } from "./usersControllers.js";
 
 const mockedUsers: UsersStructure = [
   {
@@ -41,7 +43,7 @@ describe("Given the getUsers controller middleware", () => {
 
       const next = jest.fn();
 
-      const expectedStatusCode = 200;
+      const expectedStatusCodeOk = 200;
 
       User.find = jest.fn().mockImplementationOnce(() => ({
         exec: jest.fn().mockReturnValue(mockedUsers),
@@ -53,7 +55,7 @@ describe("Given the getUsers controller middleware", () => {
         next
       );
 
-      expect(response.status).toHaveBeenCalledWith(expectedStatusCode);
+      expect(response.status).toHaveBeenCalledWith(expectedStatusCodeOk);
     });
 
     test("Then it should call its json method", async () => {
@@ -97,7 +99,7 @@ afterAll(async () => {
 describe("Given a POST 'users/register' endpoint", () => {
   describe("When it receives a request with username 'bert' and password '123456789'", () => {
     test("Then it should respond with status code '201' and message 'The user with email bert@isdi.com has been registered`", async () => {
-      const expectedStatusCode = 201;
+      const expectedStatusCodeCreated = 201;
       const expectedResponse = `The user with email ${mockedUsers[1].email} has been registered`;
 
       const requestContentHead = { "Content-Type": "multipart/form-data" };
@@ -111,7 +113,7 @@ describe("Given a POST 'users/register' endpoint", () => {
         .field("username", mockedUsers[1].username)
         .field("password", mockedUsers[1].password)
         .field("aboutme", mockedUsers[1].aboutme)
-        .expect(expectedStatusCode);
+        .expect(expectedStatusCodeCreated);
 
       expect(response.body).toStrictEqual(expectedResponse);
     });
@@ -119,5 +121,50 @@ describe("Given a POST 'users/register' endpoint", () => {
 
   afterAll(async () => {
     await User.deleteMany(mockedUsers[1]);
+  });
+});
+
+describe("Given a POST 'users/login' endpoint", () => {
+  describe("When it receives a login request with username 'bert' and password '123456789'", () => {
+    test("Then it should respond with status code '200' and its json method with a token", async () => {
+      const expectedStatusCodeOk = 200;
+      const mockedHasedPasswordCompareResult = true;
+
+      const request = {} as Request;
+
+      const response = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn(),
+      } as unknown as Response<unknown>;
+
+      const expectedToken = {
+        token:
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2M2ZiYjEyZDM4YzVlYzRhYTliYTE4NTUiLCJ1c2V",
+      };
+
+      request.body = {
+        username: mockedUsers[1].username,
+        password: mockedUsers[1].password,
+      };
+
+      const next = jest.fn();
+
+      User.findOne = jest.fn().mockImplementationOnce(() => ({
+        exec: jest.fn().mockReturnValue({
+          ...request.body,
+          _id: new mongoose.Types.ObjectId(),
+        }),
+      }));
+
+      bcrypt.compare = jest
+        .fn()
+        .mockResolvedValue(mockedHasedPasswordCompareResult);
+      jwt.sign = jest.fn().mockReturnValue(expectedToken.token);
+
+      await loginUser(request, response as Response, next);
+
+      expect(response.status).toHaveBeenCalledWith(expectedStatusCodeOk);
+      expect(response.json).toHaveBeenCalledWith(expectedToken);
+    });
   });
 });
